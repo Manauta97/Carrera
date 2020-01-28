708c62a451a55483bbece06a40c021a18a6c1326
Raúl <alumno@aula1-03>
2020-01-28 10:46:01 +0100

    empezar test

---
+++ carrera/src/carrera/TestCarrera.java
@@ -0,0 +1,14 @@
+package carrera;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCarrera {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
